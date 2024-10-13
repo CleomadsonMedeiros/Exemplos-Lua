64cfc62723c09fb1896096e17d253adef2d82305
CleomadsonMedeiros <cleomadsonmedeiros@gmail.com>
2024-10-13 19:03:12 -0300

    Atualização

---
+++ Conteudos/cleomadson_heranca_multipla.lua
@@ -0,0 +1,13 @@
+Button = {
+    super = { Shape, EventResponser }
+}
+
+
+Button.__index = function (t, k)
+    local class
+    for _, class in ipairs(Button.super) do
+        if class[k] then return class[k] end
+    end
+end
+
+-- Código feito por Cacilhas
